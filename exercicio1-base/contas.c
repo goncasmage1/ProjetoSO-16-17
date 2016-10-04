54365e7c1763985e91b33e29371ef3b58cbaacd2
Goncalao <goncalo.cool@gmail.com>
2016-10-04 11:22:38 +0100

    ficheiros iniciais

---
+++ exercicio1-base/contas.c
@@ -0,0 +1,75 @@
+#include "contas.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define atrasar() sleep(ATRASO)
+			 
+int contasSaldos[NUM_CONTAS];
+
+
+int contaExiste(int idConta) {
+  return (idConta > 0 && idConta <= NUM_CONTAS);
+}
+
+void inicializarContas() {
+  int i;
+  for (i=0; i<NUM_CONTAS; i++)
+	contasSaldos[i] = 0;
+}
+
+int debitar(int idConta, int valor) {
+  atrasar();
+  if (!contaExiste(idConta))
+	return -1;
+  if (contasSaldos[idConta - 1] < valor)
+	return -1;
+  atrasar();
+  contasSaldos[idConta - 1] -= valor;
+  return 0;
+}
+
+int creditar(int idConta, int valor) {
+	atrasar();
+	if (!contaExiste(idConta))
+		return -1;
+	contasSaldos[idConta - 1] += valor;
+	return 0;
+}
+
+int lerSaldo(int idConta) {
+	atrasar();
+	if (!contaExiste(idConta))
+		return -1;
+	return contasSaldos[idConta - 1];
+}
+
+
+void simular(int numAnos) {
+	int PID, state;
+	PID = fork();
+
+	if (PID == 0) {
+		simulacao(numAnos);
+	} else {
+		return;
+	}
+}
+
+void simulacao (int numAnos){
+	int novosSaldos[NUM_CONTAS], i, j, k;
+
+	for (k = 0; k < NUM_CONTAS; k++)
+		novosSaldos[k] = contasSaldos[k];
+
+	for (i = 0; i < numAnos; i++) {
+		printf("SIMULACAO: Ano %d\n=================\n", numAnos );
+
+		for (j = 0; j < NUM_CONTAS; j++) {
+			printf("Conta %d, Saldo %d \n ", j + 1, novosSaldos[j]);
+			novosSaldos[j] += novosSaldos[j] * TAXAJURO - CUSTOMANUTENCAO;
+		}
+		puts();
+	}
+
+}
