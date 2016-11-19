afb223a15a2d6a6515255d6c96c23877fac8673a
Goncalao <goncalo.cool@gmail.com>
2016-11-19 15:46:06 +0000

    Its something

---
+++ exercicio1-base/i-banco-terminal.c
@@ -0,0 +1,148 @@
+/*
+// Projeto SO - exercicio 1, version 1
+// Sistemas Operativos, DEI/IST/ULisboa 2016-17
+*/
+
+#include "commandlinereader.h"
+#include "contas.h"
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define COMANDO_DEBITAR "debitar"
+#define COMANDO_CREDITAR "creditar"
+#define COMANDO_LER_SALDO "lerSaldo"
+#define COMANDO_TRANSFERIR "transferir"
+#define COMANDO_SIMULAR "simular"
+#define COMANDO_SAIR "sair"
+#define COMANDO_SAIR_AGORA "agora"
+#define COMANDO_SAIR_TERMINAL "sair-terminal"
+
+#define DEBITAR 1
+#define CREDITAR 2
+#define LER_SALDO 3
+#define TRANSFERIR 4
+
+#define MAXARGS 4
+#define BUFFER_SIZE 100
+#define CMD_BUFFER_DIM  (NUM_TRABALHADORAS * 2)
+
+
+/**Variaveis globais*/
+
+char *args[MAXARGS + 1];
+char *ficheiro;
+char buffer[BUFFER_SIZE];
+
+/**Variaveis globais*/
+
+
+int main (int argc, char** argv) {
+
+	if (argc == 2) {
+		ficheiro = strdup(argv[1]);
+	}
+
+	printf("Bem-vinda/o ao i-banco\n\n");
+
+	while (1) {
+
+		int numargs = readLineArguments(args, MAXARGS + 1, buffer, BUFFER_SIZE);
+
+		if (numargs == 0)
+			/* Nenhum argumento; ignora e volta a pedir */
+			continue;
+
+		/* EOF (end of file) do stdin ou comando "sair-terminal" */
+		else if (numargs < 0 ||
+			(numargs > 0 && (strcmp(args[0], COMANDO_SAIR_TERMINAL) == 0))) {
+
+			puts("\ni-banco-terminal terminou.");
+			return 0;
+		}
+			
+		/* Debitar */
+		else if (strcmp(args[0], COMANDO_DEBITAR) == 0) {
+
+			if (numargs < 3) {
+				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_DEBITAR);
+				continue;
+			}
+			novaTarefa(DEBITAR, atoi(args[1]), -1, atoi(args[2]), 0);
+		}
+
+		/* Creditar */
+		else if (strcmp(args[0], COMANDO_CREDITAR) == 0) {
+			if (numargs < 3) {
+				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_CREDITAR);
+				continue;
+			}
+			novaTarefa(CREDITAR, atoi(args[1]), -1, atoi(args[2]), 0);
+		}
+
+		/* Ler Saldo */
+		else if (strcmp(args[0], COMANDO_LER_SALDO) == 0) {
+			if (numargs < 2) {
+				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_LER_SALDO);
+				continue;
+			}
+			novaTarefa(LER_SALDO, atoi(args[1]), -1, 0, 0);
+		}
+
+		/* Transferir */
+		else if (strcmp(args[0], COMANDO_TRANSFERIR) == 0) {
+			if (numargs < 4) {
+				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_TRANSFERIR);
+				continue;
+			}
+			novaTarefa(TRANSFERIR, atoi(args[1]), atoi(args[2]), atoi(args[3]), 1);
+		}
+
+		/* Simular */
+		else if (strcmp(args[0], COMANDO_SIMULAR) == 0) {
+
+
+			int anos = atoi(args[1]);
+
+			/*Se o numero de anos for valido*/
+			if (anos > 0) {
+
+				/*Aguarda que todas as tarefas que estao a ser executadas terminem*/
+				espera = 1;
+				while (contadorTarefas > 0 || buff_write_idx != buff_read_idx) {
+					pthread_cond_wait(&pode_simular, &mutex_cond);
+				}
+				/*Cria um processo filho*/
+				pid_t pid = fork();
+
+				/*O processo filho faz a simulacao*/
+				if (pid == 0) {
+					simular(anos);
+					exit(0);
+				}
+				/*O processo pai adiciona o pid do
+				processo filho ao vetor de pid's */
+				else if (pid > 0){
+					processos[indice++] = pid;
+
+					/*Indica que as tarefas podem resumir as operacoes*/
+					espera = 0;
+				}
+				else {
+					puts("Erro a criar o processo filho");
+				}
+			}
+			else 
+				printf("Numero de anos invalido\n");
+			
+		} else {
+			printf("Comando desconhecido. Tente de novo.\n");
+		}
+	}
+	return 0;
+}
--- exercicio1-base/i-banco.c
+++ exercicio1-base/i-banco.c
@@ -180,129 +180,7 @@ int main (int argc, char** argv) {
 	printf("Bem-vinda/o ao i-banco\n\n");
 
 	while (1) {
-
-		int numargs = readLineArguments(args, MAXARGS + 1, buffer, BUFFER_SIZE);
-
-		if (numargs == 0)
-			/* Nenhum argumento; ignora e volta a pedir */
-			continue;
-
-		/* EOF (end of file) do stdin ou comando "sair" */
-		else if (numargs < 0 ||
-			(numargs > 0 && (strcmp(args[0], COMANDO_SAIR) == 0))) {
-
-			puts("i-banco vai terminar.\n--");
-			int i, pid, status;
-			para_sair = 1;
-
-			/*Forca todas as tarefas bloqueadas a avancar e terminarem-se*/
-			for (i = 0; i < NUM_TRABALHADORAS; i++) {
-				sem_post(&sem_ler);
-			}
-			/*Certifica-se de que todas as tarefas terminaram*/
-			for (i = 0; i < NUM_TRABALHADORAS; i++) {
-				pthread_join(tid[i], NULL);
-			}
-
-			/*Sair agora - chama kill a todos os processos filho*/
-			if (args[1] != NULL && (strcmp(args[1], COMANDO_SAIR_AGORA) == 0)) {
-				for (i = 0; i < indice; i++) {
-					kill(processos[i], SIGUSR1);
-				}
-			}
-
-			/*Termina os processos zombie antes de o programa acabar*/
-			for (i = 0; i < indice; i++) {
-				pid = wait(&status);
-				printf("FILHO TERMINADO (PID=%d; ", pid);
-
-				if (status == 0) {
-					puts("terminou normalmente)");
-				} else {
-					puts("terminou abruptamente)");
-				}
-			}
-			puts("--\ni-banco terminou.");
-			return 0;
-		}
-			
-		/* Debitar */
-		else if (strcmp(args[0], COMANDO_DEBITAR) == 0) {
-
-			if (numargs < 3) {
-				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_DEBITAR);
-				continue;
-			}
-			novaTarefa(DEBITAR, atoi(args[1]), -1, atoi(args[2]), 0);
-		}
-
-		/* Creditar */
-		else if (strcmp(args[0], COMANDO_CREDITAR) == 0) {
-			if (numargs < 2) {
-				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_CREDITAR);
-				continue;
-			}
-			novaTarefa(CREDITAR, atoi(args[1]), -1, atoi(args[2]), 0);
-		}
-
-		/* Ler Saldo */
-		else if (strcmp(args[0], COMANDO_LER_SALDO) == 0) {
-			if (numargs < 2) {
-				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_LER_SALDO);
-				continue;
-			}
-			novaTarefa(LER_SALDO, atoi(args[1]), -1, -1, 0);
-		}
-
-		/* Transferir */
-		else if (strcmp(args[0], COMANDO_TRANSFERIR) == 0) {
-			if (numargs < 4) {
-				printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_TRANSFERIR);
-				continue;
-			}
-			novaTarefa(TRANSFERIR, atoi(args[1]), atoi(args[2]), atoi(args[3]), 1);
-		}
-
-		/* Simular */
-		else if (strcmp(args[0], COMANDO_SIMULAR) == 0) {
-
-
-			int anos = atoi(args[1]);
-
-			/*Se o numero de anos for valido*/
-			if (anos > 0) {
-
-				/*Aguarda que todas as tarefas que estao a ser executadas terminem*/
-				espera = 1;
-				while (contadorTarefas > 0 || buff_write_idx != buff_read_idx) {
-					pthread_cond_wait(&pode_simular, &mutex_cond);
-				}
-				/*Cria um processo filho*/
-				pid_t pid = fork();
-
-				/*O processo filho faz a simulacao*/
-				if (pid == 0) {
-					simular(anos);
-					exit(0);
-				}
-				/*O processo pai adiciona o pid do
-				processo filho ao vetor de pid's */
-				else if (pid > 0){
-					processos[indice++] = pid;
-
-					/*Indica que as tarefas podem resumir as operacoes*/
-					espera = 0;
-				}
-				else {
-					puts("Erro a criar o processo filho");
-				}
-			}
-			else 
-				printf("Numero de anos invalido\n");
-			
-		} else {
-			printf("Comando desconhecido. Tente de novo.\n");
-		}
+		;
 	}
 	return 0;
 }
@@ -313,13 +191,20 @@ void novaTarefa(int op, int id_1, int id_2, int val, int duas_contas) {
 	sem_wait(&sem_esc);
 	pthread_mutex_lock(&mutex_esc);
 
-	/*Adiciona um comando ao buffer*/
-	cmd_buffer[buff_write_idx].operacao = op;
-	cmd_buffer[buff_write_idx].idConta_1 = id_1;
-	cmd_buffer[buff_write_idx].idConta_2 = id_2;
-	cmd_buffer[buff_write_idx].valor = val;
-	cmd_buffer[buff_write_idx].com_conta_2 = duas_contas;
-	buff_write_idx = (buff_write_idx + 1) % CMD_BUFFER_DIM;
+	FILE* file = fopen("log.txt", "a");
+	if (file) {
+		/*Adiciona um comando ao buffer*/
+		comando_t com;
+		com.operacao = op;
+		com.idConta_1 = id_1;
+		com.idConta_2 = id_2;
+		com.valor = val;
+		com.com_conta_2 = duas_contas;
+
+		fwrite(&com, sizeof(struct comando_t), 1, file);
+		//buff_write_idx = (buff_write_idx + 1) % CMD_BUFFER_DIM;
+	}
+	fclose(file);
 
 	pthread_mutex_unlock(&mutex_esc);
 	/*Incrementa o semaforo de leitura (Permite que uma tarefa leia do buffer)*/
@@ -338,32 +223,40 @@ void *recebeComandos() {
 
 		pthread_mutex_lock(&mutex_ler);
 		/*Le o comando do buffer*/
-		comando_t com = cmd_buffer[buff_read_idx];
-		buff_read_idx = (buff_read_idx + 1) % CMD_BUFFER_DIM;
-		pthread_mutex_unlock(&mutex_ler);
+		int continua = 1, leu_fich = 1;
+		FILE* file = fopen(/*i-banco-pipe*/, "a");
+		if (file) {
+			comando_t com;
+			fread(&my_record,sizeof(struct rec),1,ptr_myfile);
+			buff_read_idx = (buff_read_idx + 1) % CMD_BUFFER_DIM;
+			pthread_mutex_unlock(&mutex_ler);
 
-		int continua = 1;
-		/*Troca os indices das contas se o comando for transferir
-		e se a primeira conta tiver um indice maior que a primeira*/
-		int conta_1, conta_2;
-		if (com.com_conta_2) {
-			conta_1 = (com.idConta_1 < com.idConta_2) ? com.idConta_1 : com.idConta_2;
-			conta_2 = (com.idConta_1 < com.idConta_2) ? com.idConta_2 : com.idConta_1;
+			/*Troca os indices das contas se o comando for transferir
+			e se a primeira conta tiver um indice maior que a primeira*/
+			int conta_1, conta_2;
+			if (com.com_conta_2) {
+				conta_1 = (com.idConta_1 < com.idConta_2) ? com.idConta_1 : com.idConta_2;
+				conta_2 = (com.idConta_1 < com.idConta_2) ? com.idConta_2 : com.idConta_1;
 
-			/*Se o indice for o mesmo*/
-			if (conta_1 == conta_2) {
-				continua = 0;
+				/*Se o indice for o mesmo*/
+				if (conta_1 == conta_2) {
+					continua = 0;
+				}
+			}
+			else {
+				conta_1 = com.idConta_1;
+				conta_2 = com.idConta_2;
 			}
 		}
 		else {
-			conta_1 = com.idConta_1;
-			conta_2 = com.idConta_2;
+			leu_fich = 0;
+			puts("Erro a ler o ficheiro");
 		}
 
 		contadorTarefas++;
 		/*Verifica se as contas especificadas nao estao a ser acedidas
 		e se sao validas*/
-		if (contaExiste(conta_1) && continua) {
+		if (contaExiste(conta_1) && continua && leu_fich) {
 			sem_wait(&sem_contas[conta_1]);
 			pthread_mutex_lock(&mutex_contas[conta_1]);
 
@@ -412,12 +305,12 @@ void *recebeComandos() {
 		contadorTarefas--;
 		/*Verifica se ainda ha tarefas a executar operacoes,
 		se nao houverem liberta o trinco condicional do comando simular*/
-		if (espera && contadorTarefas == 0 && buff_write_idx == buff_read_idx) {
+		if (leu_fich && espera && contadorTarefas == 0 && buff_write_idx == buff_read_idx) {
 			pthread_cond_signal(&pode_simular);
 		}
 
 		/*Imprime erros na introducao da conta*/
-		if (!contaExiste(conta_1) || !continua){
+		if ((!contaExiste(conta_1) || !continua) && leu_fich){
 			switch(com.operacao) {
 				case DEBITAR:
 					printf("%s(%d, %d): ERRO\n\n", COMANDO_DEBITAR, com.idConta_1, com.valor);
@@ -440,6 +333,7 @@ void *recebeComandos() {
 					break;
 			}
 		}
+		fclose(file);
 
 		/*Incrementa o semaforo de escrita (Indica que o buffer tem mais um espaco livre)*/
 		sem_post(&sem_esc);
@@ -448,40 +342,46 @@ void *recebeComandos() {
 }
 
 void tarefaDebitar(comando_t comando) {
-	FILE* file = fopen("log.txt", "ab");
+	FILE* file = fopen("log.txt", "a");
 	if (debitar (comando.idConta_1, comando.valor) < 0)
-		fprintf(file, "%s(%d, %d): ERRO\n\n", COMANDO_DEBITAR, comando.idConta_1, comando.valor);
+		//printf("%s(%d, %d): ERRO\n\n", COMANDO_DEBITAR, comando.idConta_1, comando.valor);
 	else
-		fprintf(file, "%s(%d, %d): OK\n\n", COMANDO_DEBITAR, comando.idConta_1, comando.valor);
+		fprintf(file, "%d: %s\n", gettid(), COMANDO_DEBITAR);
+		//printf("%s(%d, %d): OK\n\n", COMANDO_DEBITAR, comando.idConta_1, comando.valor);
 	fclose(file);
 }
 
 void tarefaCreditar(comando_t comando) {
-	FILE* file = fopen("log.txt", "ab");
+	FILE* file = fopen("log.txt", "a");
 	if (creditar (comando.idConta_1, comando.valor) < 0)
-		fprintf(file, "%s(%d, %d): Erro\n\n", COMANDO_CREDITAR, comando.idConta_1, comando.valor);
+		//printf("%s(%d, %d): Erro\n\n", COMANDO_CREDITAR, comando.idConta_1, comando.valor);
 	else
-		fprintf(file, "%s(%d, %d): OK\n\n", COMANDO_CREDITAR, comando.idConta_1, comando.valor);
+		fprintf(file, "%d: %s\n", gettid(), COMANDO_CREDITAR);
+		//printf("%s(%d, %d): OK\n\n", COMANDO_CREDITAR, comando.idConta_1, comando.valor);
 	fclose(file);
 }
 
 void tarefaLerSaldo(comando_t comando) {
-	FILE* file = fopen("log.txt", "ab");
+	FILE* file = fopen("log.txt", "a");
 	int saldo = lerSaldo(comando.idConta_1);
 	if (saldo < 0)
-		fprintf(file, "%s(%d): Erro.\n\n", COMANDO_LER_SALDO, comando.idConta_1);
+		//printf("%s(%d): Erro.\n\n", COMANDO_LER_SALDO, comando.idConta_1);
 	else
-		fprintf(file, "%s(%d): O saldo da conta é %d.\n\n", COMANDO_LER_SALDO, comando.idConta_1, saldo);
+		fprintf(file, "%d: %s\n", gettid(), COMANDO_LER_SALDO);
+		//printf("%s(%d): O saldo da conta é %d.\n\n", COMANDO_LER_SALDO, comando.idConta_1, saldo);
 	fclose(file);
 }
 
 void tarefaTransferir(comando_t comando) {
-	FILE* file = fopen("log.txt", "ab");
-	if (transferir(comando.idConta_1, comando.idConta_2, comando.valor) < 0) {
-		fprintf(file, "Erro ao transferir %d da conta %d para a conta %d.\n\n", comando.valor, comando.idConta_1, comando.idConta_2);
-	}
-	else {
-		fprintf(file, "%s(%d, %d, %d): OK\n\n", COMANDO_TRANSFERIR, comando.idConta_1, comando.idConta_2, comando.valor);
-	}
+	FILE* file = fopen("log.txt", "a");
+	if (transferir(comando.idConta_1, comando.idConta_2, comando.valor) < 0)
+		//printf("Erro ao transferir %d da conta %d para a conta %d.\n\n", comando.valor, comando.idConta_1, comando.idConta_2);
+	else
+		fprintf(file, "%d: %s\n", gettid(), COMANDO_TRANSFERIR);
+		//printf("%s(%d, %d, %d): OK\n\n", COMANDO_TRANSFERIR, comando.idConta_1, comando.idConta_2, comando.valor);
 	fclose(file);
+}
+
+uint64_t gettid() {
+    return (uint64_t)pthread_self();
 }
